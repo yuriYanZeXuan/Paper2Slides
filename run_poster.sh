06f86c35dbfa0050cd50068bd4c4e9b3049e2d3b
yanzexuan <yanzexuan@xiaohongshu.com>
2025-12-09 17:44:51 +0800

    add sh

---
+++ run_poster.sh
@@ -0,0 +1,6 @@
+python -m paper2slides\
+     --input /mnt/tidalfs-bdsz01/usr/tusen/yanzexuan/PosterGen/data/Object_Pose_Estimation_with_Statistical_Guarantees_Conformal_Keypoint_Detection_and_Geometric_Uncertainty_Propagation/paper.pdf \
+     --output poster \
+     --style doraemon \
+     --length medium \
+     --fast
