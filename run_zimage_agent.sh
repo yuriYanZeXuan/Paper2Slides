1b50fd5ab4af368523f5c7fbb71b713ede7cb38d
yanzexuan <yanzexuan@xiaohongshu.com>
2025-12-12 10:51:43 +0800

    change md

--- run_zimage_agent.sh
+++ run_zimage_agent.sh
@@ -6,7 +6,7 @@
 LOCAL_IMAGE_MODEL=${LOCAL_IMAGE_MODEL:-/mnt/tidalfs-bdsz01/usr/tusen/yanzexuan/weight/Z-Image}
 
 python -m paper2slides.agents.zimage_pipeline_agent \
-  --input /mnt/tidalfs-bdsz01/usr/tusen/yanzexuan/PosterGen/data/Object_Pose_Estimation_with_Statistical_Guarantees_Conformal_Keypoint_Detection_and_Geometric_Uncertainty_Propagation/paper.pdf \
+  --input /mnt/tidalfs-bdsz01/usr/tusen/yanzexuan/postergenparserunit/examples/tusen_1210.md \
   --output poster \
   --style academic \
   --local-image-model "${LOCAL_IMAGE_MODEL}" \
