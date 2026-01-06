6128d0dbbcdd1269a5b45f8475eff83c325093e2
yanzexuan <yanzexuan@xiaohongshu.com>
2026-01-06 10:05:37 +0800

    fix sh

--- run_zimage_agent.sh
+++ run_zimage_agent.sh
@@ -2,8 +2,8 @@
 
 # 使用 Z-Image + Agent 生成 academic 风格的 poster，并使用本地权重
 # 权重路径与 run_poster.sh 中的 Z-Image 默认路径保持一致
-export LOCAL_IMAGE_HEIGHT=1280
-export LOCAL_IMAGE_WIDTH=2048
+export LOCAL_IMAGE_HEIGHT=960
+export LOCAL_IMAGE_WIDTH=1280
 
 LOCAL_IMAGE_MODEL="/mnt/tidalfs-bdsz01/usr/tusen/yanzexuan/weight/Z-Image"
 
